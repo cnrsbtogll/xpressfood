736bf6eb824c60b28826add72300e4e594a0fbdc
cnrsbtogll <cnrsbtogll@gmail.com>
2023-09-07 19:18:16 +0300

    categories added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,13 +5,18 @@ import {
   View,
   TouchableOpacity,
   ScrollView,
+  Pressable,
+  Image,
 } from 'react-native';
 import HomeHeader from '../components/HomeHeader';
 import {colors} from '../global/styles';
 import {Icon} from '@rneui/base';
+import {FlatList} from 'react-native-gesture-handler';
+import {filterData} from '../global/data';
 
 export default function HomeScreen() {
   const [delivery, setDelivery] = useState(true);
+  const [indexCheck, setIndexCheck] = useState('0');
   return (
     <View style={styles.container}>
       <HomeHeader />
@@ -50,8 +55,7 @@ export default function HomeScreen() {
           </View>
         </View>
         <View style={styles.filterView}>
-          <View
-            style={styles.addressView}>
+          <View style={styles.addressView}>
             <View style={{flexDirection: 'row', alignItems: 'center'}}>
               <Icon
                 name="map-marker"
@@ -83,6 +87,46 @@ export default function HomeScreen() {
         <View style={styles.headerTextView}>
           <Text style={styles.headerText}> Categories</Text>
         </View>
+        <View>
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={filterData}
+            keyExtractor={item => item.id}
+            extraData={indexCheck}
+            renderItem={({item, index}) => (
+              <Pressable
+                onPress={() => {
+                  setIndexCheck(item.id);
+                }}>
+                <View
+                  style={
+                    indexCheck === item.id
+                      ? {...styles.smallCardSelected}
+                      : {...styles.smallCard}
+                  }>
+                  <Image
+                    style={{height: 60, width: 60, borderRadius: 30}}
+                    source={item.image}
+                  />
+                  <View>
+                    <Text
+                      style={
+                        indexCheck === item.id
+                          ? {...styles.smallCardTextSected}
+                          : {...styles.smallCardText}
+                      }>
+                      {item.name}
+                    </Text>
+                  </View>
+                </View>
+              </Pressable>
+            )}
+          />
+        </View>
+        <View style={styles.headerTextView}>
+          <Text style={styles.headerText}> Free delivery now</Text>
+        </View>
       </ScrollView>
     </View>
   );
@@ -113,7 +157,7 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     paddingHorizontal: 5,
   },
-  addressView:{
+  addressView: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     backgroundColor: colors.grey5,
@@ -122,14 +166,42 @@ const styles = StyleSheet.create({
     padding: 4,
     flex: 9,
   },
-  headerText:{
-    color:colors.grey2,
-    fontSize:22,
-    fontWeight:'bold',
-    paddingLeft:20
+  headerText: {
+    color: colors.grey2,
+    fontSize: 22,
+    fontWeight: 'bold',
+    paddingLeft: 20,
   },
-  headerTextView:{
-    backgroundColor:colors.grey5,
-    paddingVertical:2
-  }
+  headerTextView: {
+    backgroundColor: colors.grey5,
+    paddingVertical: 2,
+  },
+  smallCard: {
+    borderRadius: 30,
+    backgroundColor: colors.cardBackground,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    width: 80,
+    margin: 10,
+    padding: 5,
+  },
+  smallCardSelected: {
+    borderRadius: 30,
+    backgroundColor: colors.buttons,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    width: 80,
+    margin: 10,
+    padding: 5,
+  },
+  smallCardTextSected: {
+    fontWeight: 'bold',
+    color: colors.cardBackground,
+  },
+  smallCardText: {
+    fontWeight: 'bold',
+    color: colors.grey2,
+  },
 });
