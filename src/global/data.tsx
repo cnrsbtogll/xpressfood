30227f5b395c449836c0a90c8d083e9fbee7ad85
cnrsbtogll <cnrsbtogll@gmail.com>
2023-09-12 08:32:43 +0300

    search screen added

--- src/global/data.tsx
+++ src/global/data.tsx
@@ -45,7 +45,8 @@ export const restaurantsData = [
     restaurantName: 'KFC',
     farAway: '12.7',
     businessAddress: '22 Bessie street, Cape Town',
-    images: 'https://d3uq8t65zitdqo.cloudfront.net/eyJidWNrZXQiOiJrZmMtaW1hZ2UtYnVja2V0Iiwia2V5IjoidXBsb2Fkc1wvaW1hZ2VzXC9hYm91dC11c1wvaGFra2ltaXpkYS1nb3JzZWwuanBnIiwiZWRpdHMiOnsid2VicCI6eyJxdWFsaXR5IjoxMDB9LCJqcGVnIjp7InF1YWxpdHkiOjEwMH19fQ==',
+    images:
+      'https://d3uq8t65zitdqo.cloudfront.net/eyJidWNrZXQiOiJrZmMtaW1hZ2UtYnVja2V0Iiwia2V5IjoidXBsb2Fkc1wvaW1hZ2VzXC9hYm91dC11c1wvaGFra2ltaXpkYS1nb3JzZWwuanBnIiwiZWRpdHMiOnsid2VicCI6eyJxdWFsaXR5IjoxMDB9LCJqcGVnIjp7InF1YWxpdHkiOjEwMH19fQ==',
     averageReview: 4.3,
     numberOfReview: 306,
     coordinates: {lat: -26.1891648, lng: 28.2441808},
@@ -77,7 +78,8 @@ export const restaurantsData = [
     restaurantName: 'Steers',
     farAway: '5',
     businessAddress: ' 17 Olivia Rd, Johannesburg',
-    images: 'https://d1ralsognjng37.cloudfront.net/eb2c979a-978d-4a95-a8a5-9953152a8b5d.jpeg',
+    images:
+      'https://d1ralsognjng37.cloudfront.net/eb2c979a-978d-4a95-a8a5-9953152a8b5d.jpeg',
     coordinates: {lat: -26.1886781, lng: 28.244879},
     averageReview: 4.9,
     numberOfReview: 1272,
@@ -109,7 +111,8 @@ export const restaurantsData = [
     restaurantName: 'Roman Pizza',
     farAway: '7',
     businessAddress: ' 15 Atlas Rd, Kempton Park',
-    images: 'https://thepizzaheaven.com/wp-content/uploads/2022/11/Roman-pizza-al-taglio.jpg',
+    images:
+      'https://thepizzaheaven.com/wp-content/uploads/2022/11/Roman-pizza-al-taglio.jpg',
     averageReview: 4.3,
     numberOfReview: 700,
     coordinates: {lat: -26.1845336, lng: 28.2481691},
@@ -135,5 +138,63 @@ export const restaurantsData = [
       },
     ],
     id: 3,
+  },
+];
+
+export const filterData2 = [
+  {
+    name: 'Fast food',
+    image:
+      'https://cf.kizlarsoruyor.com/q13077087/15b4f3d6-c432-4f50-8fa5-cf664ef9eca9.jpg',
+    id: '0',
+  },
+  {
+    name: 'Burgers',
+    image:
+      'https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/1:1/w_2560%2Cc_limit/Smashburger-recipe-120219.jpg',
+    id: '1',
+  },
+  {
+    name: 'Salads',
+    image:
+      'https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad-500x375.jpg',
+    id: '2',
+  },
+  {
+    name: 'Hotdog',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/b1/Hot_dog_with_mustard.png',
+    id: '3',
+  },
+  {
+    name: 'Chinese',
+    image: 'https://images.deliveryhero.io/image/fd-tr/LH/wgy0-hero.jpg',
+    id: '4',
+  },
+  {
+    name: 'Mexican',
+    image:
+      'https://insanelygoodrecipes.com/wp-content/uploads/2022/10/Homemade-Vegan-Mexican-Beef-Tacos-with-Herbs.jpg',
+    id: '5',
+  },
+  {
+    name: 'Sea food',
+    image: 'https://www.licious.in/blog/wp-content/uploads/2022/02/shutterstock_1773695441-min.jpg',
+    id: '6',
+  },
+  {
+    name: 'Chinese Food',
+    image: 'https://media.cnn.com/api/v1/images/stellar/prod/220921081550-05-chinese-foods-mapo-tofu.jpg?c=original',
+    id: '7',
+  },
+  {
+    name: 'Mexican pie',
+    image: 'https://hips.hearstapps.com/delish/assets/17/45/1510262160-delish-hot-chocolate-pie-1.jpg',
+    id: '8',
+  },
+  {
+    name: 'Ocean dish',
+    image: 'https://www.womenstuff.co.za/wp-content/uploads/2021/12/OB-Sea-Bream_header.jpeg',
+    id: '9',
   },
 ];
--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -3,14 +3,19 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import RootClientTabs from './ClientTab';
 import {Icon} from '@rneui/base';
 import {colors} from '../global/styles';
+import BusinessConsoleScreen from '../screens/BusinessConsoleScreen';
+import DrawerContent from '../components/DrawerContent';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator screenOptions={{
-        headerShown:false
-    }}>
+    <Drawer.Navigator
+    drawerContent= {props =><DrawerContent {...props} /> }
+      screenOptions={{
+        headerShown: false,
+      }}
+      >
       <Drawer.Screen
         name="RootClientTabs"
         component={RootClientTabs}
@@ -21,6 +26,22 @@ export default function DrawerNavigator() {
               name="home"
               type="material-commonity"
               color={focused ? '#7cc' : colors.grey2}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Business consoleScreen"
+        component={BusinessConsoleScreen}
+        options={{
+          title: 'Business console',
+          drawerIcon: ({focused, size}) => (
+            <Icon
+              type="material"
+              name="business"
+              color={focused ? '#7cc' : colors.grey2}
+              size={size}
             />
           ),
         }}
